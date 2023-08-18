adc8005c2b2bfe464cd7f0fba0f65957a370b949
sarker32 <lifeline.bs1@gmail.com>
2023-08-18 14:10:25 +0600

    Baper Bank almost ready

--- js/withdraw.js
+++ js/withdraw.js
@@ -20,19 +20,27 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
-    // step-4
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
+    
     // step-5
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    // step-7. 
+    withdrawField.value = '';
+
+    if(newWithdrawAmount > previousBalanceTotal){
+        alert('Baper Bank a eto Taka nai');
+        return;
+    }
+
+    // step-4
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
     // step- 6.     
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
 
-    // step-7. 
-    withdrawField.value = '';
+    
     
 
 })
